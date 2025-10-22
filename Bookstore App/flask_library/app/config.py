62fc3a3c7406120d8bf3925fdf6cbd54743a3843
DUC <duyduc.binham@gmail.com>
2025-10-22 22:50:26 +0700

    Change gitignore to stop tracking cache and env file. Move the secret key into the env filee

--- Bookstore App/flask_library/app/config.py
+++ Bookstore App/flask_library/app/config.py
@@ -1,7 +1,8 @@
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres:123@localhost/book'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = "1befa8d526f3b43cf2d9108fd385dccb23bce4039bbcb41c72e87d04976757866d91287193921d269d5da2898d0b07013164b128fa1c65979ad7b736eabba273"
-        
+    SECRET_KEY = os.environ.get('FLASK_LIBRARY_SECRET_KEY')
--- Bookstore App/flask_library/run.ps1
+++ Bookstore App/flask_library/run.ps1
@@ -1,8 +1,4 @@
-# Allow scripts to run in this session
-Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass -Force
-
 # Activate the virtual environment
 & "$PSScriptRoot/venv/Scripts/Activate.ps1"
-
 # Run Flask app
 python -m flask --app app run --debug
